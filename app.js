22db1658a6ac75b39cc05b1d2b3c32991550d565
amehmathias <amehmathiasejeh40@gmail.com>
2026-07-17 15:19:51 +0100

    implemented root and health endpoints

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const PORT = 3000;
 
 const app = express();
 app.disable('x-powered-by');
+
 app.get("/", (req, res) => {
     res.send("Hello  Server!");
 });
 
+
+app.get("/", (req, res) => {
+    res.send({"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]})
+});
+
+
 app.get("/health", (req, res) => {
-    res.send("OK")
-})
+    res.send({"status": "ok"})
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
