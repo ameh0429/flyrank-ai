54c81cbfcde2df58278822a0a8716a8ec5c63901
amehmathias <amehmathiasejeh40@gmail.com>
2026-08-06 20:20:00 +0100

    Stage 1: signup and login routes working

--- self-version/app.js
+++ self-version/app.js
@@ -40,6 +40,38 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
+// Signup
+app.post('/auth/signup', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Email and password are required' });
+  }
+  try {
+    const signup = await supabase.auth.signUp({
+      email: email,
+      password: password
+    });
+    res.status(200).json({ message: 'Signup successful', user: signup.data.user });
+  } catch (error) {
+    res.status(500).json({ error: 'Error occurred during signup' });
+  }
+});
+
+// Login
+app.post('/auth/login', async (req, res) => {
+  const {email, password } = req.body;
+  if(!email || !password) {
+    return res.status(400).json({ error: 'Email and password are required' })
+  }
+  const {data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+  if (error) {
+    return res.status(401).json({error: 'Invalid login credentials'})
+  }
+
+  res.status(200).json({mesage: 'Login successful', access_token: data.session.access_token, refresh_token: data.session.refresh_token})
+})
+
 // Get all tasks
 app.get("/tasks", async (req, res) => {
   try {
